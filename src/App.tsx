0d1a77e748ed239ccbc2952f6d024624bf71e44e
Thomas DOUCHE <Thomas.douche@aol.com>
2023-01-05 21:18:49 +0100

    First version

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@mui/joy/Typography';
+import Stack from '@mui/joy/Stack';
+import Button from '@mui/joy/Button';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Stack justifyContent="center" alignItems="center" spacing={1}>
+        <Typography level="h3">Welcome to CycleChain</Typography>
+        <Button>Connect</Button>
+      </Stack>
   );
 }
 
